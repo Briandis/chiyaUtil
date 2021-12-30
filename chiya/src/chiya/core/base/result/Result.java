17d80ca39cc14d026cb3d7f511c017ee01fb8bcf
韦东 <wei895926300@126.com>
2021-12-30 18:03:50 +0800

    修订了返回值的静态枚举方法

--- chiya/src/chiya/core/base/result/Result.java
+++ chiya/src/chiya/core/base/result/Result.java
@@ -6,7 +6,6 @@ import chiya.core.base.page.Page;
  * HTTP默认装配的相应信息
  * 
  * @author Brian
- *
  */
 public class Result {
 
@@ -72,34 +71,74 @@ public class Result {
 		this.message = resultEnum.getMessage();
 	}
 
+	/**
+	 * 获取状态码
+	 * 
+	 * @return code
+	 */
 	public int getCode() {
 		return code;
 	}
 
+	/**
+	 * 获取数据体
+	 * 
+	 * @return data体
+	 */
 	public Object getData() {
 		return data;
 	}
 
+	/**
+	 * 获取消息
+	 * 
+	 * @return message
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * 设置状态码
+	 * 
+	 * @param code 状态码
+	 */
 	public void setCode(int code) {
 		this.code = code;
 	}
 
+	/**
+	 * 设置内容
+	 * 
+	 * @param data 内容
+	 */
 	public void setData(Object data) {
 		this.data = data;
 	}
 
+	/**
+	 * 设置消息
+	 * 
+	 * @param message 消息
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
+	/**
+	 * 获取分页
+	 * 
+	 * @return 分页对象
+	 */
 	public Page getPage() {
 		return page;
 	}
 
+	/**
+	 * 设置分页
+	 * 
+	 * @param page 分页对象
+	 */
 	public void setPage(Page page) {
 		this.page = page;
 	}
@@ -199,4 +238,15 @@ public class Result {
 	public static Result judge(boolean isSuccess, Object data, Page page) {
 		return isSuccess ? success(data, page) : fail();
 	}
+
+	/**
+	 * 根据枚举值响应
+	 * 
+	 * @param resultEnum 枚举值
+	 * @return Result 消息对象
+	 */
+	public static Result enums(ResultEnum resultEnum) {
+		return new Result(resultEnum);
+	}
+
 }
