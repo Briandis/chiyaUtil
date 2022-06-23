bb11497d6b28833e4ee88be0b682db098ecaad08
chiya <wei895926300@126.com>
2022-06-23 16:30:05 +0800

    部分库底层修订

---
+++ chiya/src/chiya/core/base/function/GenericityFunction.java
@@ -0,0 +1,18 @@
+package chiya.core.base.function;
+
+/**
+ * 泛型获取对象
+ * 
+ * @author brain
+ * @param <T> 泛型
+ *
+ */
+public interface GenericityFunction<T> {
+
+	/**
+	 * 获取下一个泛型对象
+	 * 
+	 * @param t 泛型对象
+	 */
+	void next(T t);
+}
